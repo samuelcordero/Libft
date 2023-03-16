6e19e6ee8a96b899b0aa3d2ebca69e965c7a19f1
Samuel Cordero Rodrigo <sacorder@c1r16s5.42madrid.com>
2023-03-16 18:38:51 +0100

    simplified atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,23 +6,14 @@
 /*   By: sacorder <sacorder@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 13:49:40 by sacorder          #+#    #+#             */
-/*   Updated: 2023/03/10 15:31:34 by sacorder         ###   ########.fr       */
+/*   Updated: 2023/03/16 18:38:18 by sacorder         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 static int	ft_is_space(char c)
 {
-	if (c == ' ')
-		return (1);
-	else if (c == '\v')
-		return (1);
-	else if (c == '\r')
-		return (1);
-	else if (c == '\t')
-		return (1);
-	else if (c == '\f')
-		return (1);
-	else if (c == '\n')
+	if (c == ' ' || c == '\v' || c == '\r' || c == '\t' || c == '\f'
+		|| c == '\n')
 		return (1);
 	else
 		return (0);
@@ -37,9 +28,7 @@ int	ft_atoi(const char *str)
 	n = 0;
 	sign = 1;
 	i = 0;
-	if (str[0] == '\0')
-		return (0);
-	while (str[i] != '-' && str[i] != '+' && ft_is_space(str[i]))
+	while (ft_is_space(str[i]))
 		++i;
 	if (str[i] == '-' || str[i] == '+')
 	{
