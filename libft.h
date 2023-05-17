b163843345a089b638091c0c616c147649e3c30b
Samuel Cordero <sacorder@student.42madrid.com>
2023-05-17 13:04:44 +0200

    updated libft, now includes get_next_line and ft_printf, use make bonus to compile!!!

--- libft.h
+++ libft.h
@@ -13,6 +13,12 @@
 # define LIBFT_H
 
 # include <stddef.h>
+# include <stdarg.h>
+# include <unistd.h>
+# include <limits.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
 
 typedef struct s_list
 {
@@ -68,4 +74,12 @@ t_list	*ft_lstlast(t_list *lst);
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 t_list	*ft_lstnew(void *content);
 int		ft_lstsize(t_list *lst);
+
+// GET NEXT LINE //
+
+char	*get_next_line(int fd);
+
+// PRINTF //
+
+int	ft_printf(char const *str, ...);
 #endif
